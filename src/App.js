a95e495ee027b58c465ac9014cfb4ea8d90af65d
ARJUN-TUDU <64701428+ARJUN-TUDU@users.noreply.github.com>
2024-02-21 20:59:03 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import {useEffect,useState} from 'react';
+import MyMap from './MyMap';
+import Practice_api_1 from './Practice_api_1';
 
+import {BrowserRouter,Link,Routes,Route} from 'react-router-dom'
+import Order from './Order';
 function App() {
+  
+ 
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <BrowserRouter>
+     
+     <Routes>
+
+        <Route path = "/home" element = {<Practice_api_1/>} />
+        <Route path = "/Orders/:items" element = {<Order/>}/>
+
+
+
+     </Routes>
+     
+     
+     </BrowserRouter>
   );
 }
 
